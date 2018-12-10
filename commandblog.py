a02d16d31eacdb50f62789e5cbf1321faefacb11
IssaIan <issamwangi@gmail.com>
2018-12-10 15:08:42 +0300

    [starts #162539042]Add moderator user edit comment

--- commandblog.py
+++ commandblog.py
@@ -36,7 +36,7 @@ def login():
             # return user['password']
             password = input("please input password: ")
             if user['password'] != password:
-                print('Wrong password')
+                return 'Wrong password'
             user["lastLoginAt"] = datetime.datetime.now()
 
     if user['role'] == "normal":
@@ -45,15 +45,70 @@ def login():
         if userinput == str("1"):
             comment = input("Enter your comment:")
         
-        data = {'comment_id': len(comments) +1,
+            data = {'comment_id': len(comments) +1,
             'comment': comment,
             'timestamp': datetime.datetime.now() ,
             'created_by': username
             }
-        comments.append(data)
-        return comments
+            comments.append(data)
+            return comments
 
+        elif userinput == str("2"):
+            comment_id = int(input('Enter comment id:'))
+            if not comment_id:
+                return "Enter comment id"
+            comment = next((comment for comment in comments if comment["comment_id"] == comment_id), False)
+            if comment == False:
+                    return "No comment found"
+            edit = input("Enter your comment here:")
+            comment["comment"] = edit
+            return comments
+
+        else:
+            login()
+
+    
+    if user['role'] == "moderator":
+        userinput = input("1. create comment \n 2. edit comment \n 3. delete comment \n 4. logout \n ")
         
+        if userinput == str("1"):
+            comment = input("Enter your comment:")
+        
+            data = {'comment_id': len(comments) +1,
+            'comment': comment,
+            'timestamp': datetime.datetime.now() ,
+            'created_by': username
+            }
+            comments.append(data)
+            return comments
+
+        elif userinput == str("2"):
+            comment_id = int(input('Enter comment id:'))
+            if not comment_id:
+                return "Enter comment id: "
+            comment = next((comment for comment in comments if comment["comment_id"] == comment_id), False)
+            if comment == False:
+                    return "No comment found"
+            edit = input("Enter your comment here:")
+            comment["comment"] = edit
+            return comments
+        elif userinput == str("3"):
+            comment_id = int(input('Enter comment id'))
+            if not comment_id:
+                return 'Enter comment id'
+            comment = next((comment for comment in comments if comment["comment_id"] == comment_id), False)
+            if comment == False:
+                    return "No comment found"
+            comments.remove(comment)
+            return comments
+
+
+        else:
+            login()
+
+
+
+            
 
 
 print(login())
